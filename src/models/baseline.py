ad49e647394d1139e71f059a3a022fc1bcc5d9d0
boubacar-sow <72445986+boubacar-sow@users.noreply.github.com>
2024-01-20 17:50:39 +0100

    add GCNModel

--- src/models/baseline.py
+++ src/models/baseline.py
@@ -33,6 +33,52 @@ class BaseGraphEncoder(nn.Module):
         x = self.mol_hidden2(x)
         return x
 
+class GCNModel(nn.Module):
+    def __init__(self, num_node_features, nout, nhid, graph_hidden_channels):
+        super(GCNModel, self).__init__()
+        
+
+        self.nhid = nhid
+        self.nout = nout
+
+        self.temp = nn.Parameter(torch.Tensor([0.07]))
+        self.register_parameter( 'temp' , self.temp )
+        
+        self.ln1 = nn.LayerNorm((nout))
+        self.ln2 = nn.LayerNorm((nout))
+
+        self.relu = nn.ReLU()
+        
+        self.conv1 = GCNConv(num_node_features, graph_hidden_channels)
+        self.conv2 = GCNConv(graph_hidden_channels, graph_hidden_channels)
+        self.conv3 = GCNConv(graph_hidden_channels, graph_hidden_channels)
+        self.mol_hidden1 = nn.Linear(graph_hidden_channels, nhid)
+        self.mol_hidden2 = nn.Linear(nhid, nhid)
+        self.mol_hidden3 = nn.Linear(nhid, nout)
+
+
+    def forward(self, graph_batch):
+      
+        x = graph_batch.x
+        edge_index = graph_batch.edge_index
+        batch = graph_batch.batch
+        x = self.conv1(x, edge_index)
+        x = x.relu()
+        x = self.conv2(x, edge_index)
+        x = x.relu()
+        x = self.conv3(x, edge_index)
+
+        x = global_mean_pool(x, batch)  
+
+        
+        x = self.mol_hidden1(x).relu()
+        x = self.mol_hidden2(x).relu()
+        x = self.mol_hidden3(x)
+        
+        x = self.ln1(x)
+        x = x * torch.exp(self.temp)
+
+        return x
 
 class BaseTextEncoder(nn.Module):
     def __init__(self, model_name):
@@ -50,7 +96,7 @@ class Baseline(nn.Module):
         self, model_name, num_node_features, nout, nhid, graph_hidden_channels
     ):
         super(Baseline, self).__init__()
-        self.graph_encoder = BaseGraphEncoder(
+        self.graph_encoder = GCNModel(
             num_node_features, nout, nhid, graph_hidden_channels
         )
         self.text_encoder = BaseTextEncoder(model_name)
