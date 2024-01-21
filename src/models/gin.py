07f089fbae6f2f9daa60ac92d89d09fadfd9bd91
Ali Ramlaoui <ramlaoui@outlook.com>
2024-01-21 13:51:03 +0100

    modify gin model

--- src/models/gin.py
+++ src/models/gin.py
@@ -1,14 +1,27 @@
+import torch
 from torch import nn
 import torch.nn.functional as F
 
 from torch_geometric.nn import GINConv
 from torch_geometric.nn import global_mean_pool
+from torch_geometric.utils import to_dense_batch
 from transformers import AutoModel
 
 
-class BaseGraphEncoder(nn.Module):
-    def __init__(self, num_node_features, nout, nhid, nlayers, graph_hidden_channels):
-        super(BaseGraphEncoder, self).__init__()
+class GINEncoder(nn.Module):
+    def __init__(
+        self,
+        num_node_features,
+        nout,
+        nhid,
+        nlayers,
+        graph_hidden_channels,
+        skip_connection="last",
+        type_model="encoder",
+    ):
+        super(GINEncoder, self).__init__()
+        self.skip_connection = skip_connection
+        self.type_model = type_model
         self.nhid = nhid
         self.nout = nout
         self.relu = nn.ReLU()
@@ -33,7 +46,6 @@ class BaseGraphEncoder(nn.Module):
                         nn.Linear(2 * graph_hidden_channels, graph_hidden_channels),
                     ),
                     train_eps=True,
-                    aggr="add",
                 )
             )
 
@@ -44,6 +56,7 @@ class BaseGraphEncoder(nn.Module):
         self.projection1 = nn.Linear(graph_hidden_channels, nhid)
         self.projection2 = nn.Linear(nhid, nout)
         self.dropout = nn.Dropout(0.1)
+        self.ln = nn.LayerNorm((graph_hidden_channels))
 
         # self.gin = GIN(
         #     num_node_features,
@@ -57,19 +70,47 @@ class BaseGraphEncoder(nn.Module):
         x = graph_batch.x
         edge_index = graph_batch.edge_index
         batch = graph_batch.batch
-        x = self.layers[0](x, edge_index).relu()
-        x = self.dropout(x)
-        x = self.bns[0](x)
-        for i, layer in enumerate(self.layers[1:]):
-            x = layer(x, edge_index).relu()
-            x = self.dropout(x)
-            x = self.bns[i](x)
-        x = global_mean_pool(x, batch)
 
-        x = self.projection1(x).relu()
+        h_list = [x]
+        for i, layer in enumerate(self.layers):
+            h = layer(h_list[-1], edge_index)
+            h = self.bns[i](h)
+            if i == len(self.layers) - 1:
+                h = self.dropout(h)
+            else:
+                h = self.dropout(self.relu(h))
+            h_list.append(h)
+
+        if self.skip_connection == "last":
+            node_representation = h_list[-1]
+        elif self.skip_connection == "sum":
+            node_representation = torch.sum(torch.cat(h_list, dim=0), dim=0)[0]
+
+        h_graph = global_mean_pool(node_representation, batch)
+
+        x = self.projection1(h_graph).relu()
         x = self.dropout(x)
         x = self.projection2(x)
-        return x
+
+        if self.type_model == "qformer":
+            batch_node, batch_mask = to_dense_batch(node_representation, batch)
+            batch_mask = batch_mask.bool()
+
+            batch_node = torch.cat((h_graph.unsqueeze(1), batch_node), dim=1)
+            batch_mask = torch.cat(
+                (
+                    torch.ones(
+                        (batch_mask.shape[0], 1), dtype=torch.bool, device=batch.device
+                    ),
+                    batch_mask,
+                ),
+                dim=1,
+            )
+
+            batch_node = self.ln(batch_node)
+            return batch_node, batch_mask, x
+        else:
+            return x
 
 
 class BaseTextEncoder(nn.Module):
@@ -95,7 +136,7 @@ class GINMol(nn.Module):
         self, model_name, num_node_features, nout, nhid, nlayers, graph_hidden_channels
     ):
         super(GINMol, self).__init__()
-        self.graph_encoder = BaseGraphEncoder(
+        self.graph_encoder = GINEncoder(
             num_node_features, nout, nhid, nlayers, graph_hidden_channels
         )
         self.text_encoder = BaseTextEncoder(model_name, nout, nhid)
